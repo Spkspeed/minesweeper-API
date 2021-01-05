9b2cb4f870d846aae9f48819c8f934be38b9faa0
recruz79 <raul.cruz79@gmail.com>
2021-01-05 20:34:27 -0300

    Added log4j error prints

--- src/main/java/minesweeper/service/MinesweeperService.java
+++ src/main/java/minesweeper/service/MinesweeperService.java
@@ -1,6 +1,8 @@
 package minesweeper.service;
 
 import minesweeper.exception.MinesweeperException;
+import minesweeper.exception.RestResponseEntityExceptionHandler;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ public class MinesweeperService {
 
     @Autowired
     MinesweeperPersistService minesweeperPersistService;
+
+    Logger log = Logger.getLogger(MinesweeperService.class);
+
 
     protected HashMap<String, MineGameGrid> gameInstances = new HashMap();
 
@@ -69,6 +74,7 @@ public class MinesweeperService {
     }
 
     public void saveGame(String user) throws MinesweeperException {
+        log.debug("El problema es que devuelve: " + gameInstances.get(user));
         minesweeperPersistService.saveGame(user, gameInstances.get(user));
     }
 
