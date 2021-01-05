fa3c7a77dd51f91ca3d4961937e68fad4917c941
recruz79 <raul.cruz79@gmail.com>
2021-01-05 20:39:21 -0300

    Added log4j error prints

--- src/main/java/minesweeper/service/MinesweeperService.java
+++ src/main/java/minesweeper/service/MinesweeperService.java
@@ -75,6 +75,7 @@ public class MinesweeperService {
 
     public void saveGame(String user) throws MinesweeperException {
         log.debug("El problema es que devuelve: " + gameInstances.get(user));
+        log.debug("ERROR" + minesweeperPersistService);
         minesweeperPersistService.saveGame(user, gameInstances.get(user));
     }
 
